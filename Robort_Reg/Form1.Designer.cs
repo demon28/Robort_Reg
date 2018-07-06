13013d8e5767e554556a3e4fa371a32d00961958
demon28 <274733956@qq.com>
2018-07-06 23:02:37 +0800

    gith

--- Robort_Reg/Form1.Designer.cs
+++ Robort_Reg/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_jiami = new System.Windows.Forms.Button();
+            this.btn_jiemi = new System.Windows.Forms.Button();
+            this.tb_jiami = new System.Windows.Forms.TextBox();
+            this.tb_jiemi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_jiami
+            // 
+            this.btn_jiami.Location = new System.Drawing.Point(238, 127);
+            this.btn_jiami.Name = "btn_jiami";
+            this.btn_jiami.Size = new System.Drawing.Size(75, 23);
+            this.btn_jiami.TabIndex = 0;
+            this.btn_jiami.Text = "加 密";
+            this.btn_jiami.UseVisualStyleBackColor = true;
+            this.btn_jiami.Click += new System.EventHandler(this.btn_jiami_Click);
+            // 
+            // btn_jiemi
+            // 
+            this.btn_jiemi.Location = new System.Drawing.Point(22, 127);
+            this.btn_jiemi.Name = "btn_jiemi";
+            this.btn_jiemi.Size = new System.Drawing.Size(75, 23);
+            this.btn_jiemi.TabIndex = 1;
+            this.btn_jiemi.Text = "解 密";
+            this.btn_jiemi.UseVisualStyleBackColor = true;
+            this.btn_jiemi.Click += new System.EventHandler(this.btn_jiemi_Click);
+            // 
+            // tb_jiami
+            // 
+            this.tb_jiami.Location = new System.Drawing.Point(12, 12);
+            this.tb_jiami.Multiline = true;
+            this.tb_jiami.Name = "tb_jiami";
+            this.tb_jiami.Size = new System.Drawing.Size(309, 99);
+            this.tb_jiami.TabIndex = 2;
+            // 
+            // tb_jiemi
+            // 
+            this.tb_jiemi.Location = new System.Drawing.Point(12, 179);
+            this.tb_jiemi.Multiline = true;
+            this.tb_jiemi.Name = "tb_jiemi";
+            this.tb_jiemi.Size = new System.Drawing.Size(309, 99);
+            this.tb_jiemi.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(353, 323);
+            this.Controls.Add(this.tb_jiemi);
+            this.Controls.Add(this.tb_jiami);
+            this.Controls.Add(this.btn_jiemi);
+            this.Controls.Add(this.btn_jiami);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_jiami;
+        private System.Windows.Forms.Button btn_jiemi;
+        private System.Windows.Forms.TextBox tb_jiami;
+        private System.Windows.Forms.TextBox tb_jiemi;
     }
 }
 
--- Robort_Reg/Form1.cs
+++ Robort_Reg/Form1.cs
@@ -12,9 +12,30 @@ namespace Robort_Reg
 {
     public partial class Form1 : Form
     {
+        string dekey = @"<RSAKeyValue><Modulus>zXRigE1lYYiyv6pin5nt7hxkHvbVg3kjzUQpgBN+SCqNZJ4cL03L5vo2F0/gqBds5Xq183SPbD5VD+EgRd8na1QvWypv9mjY2DHyNEBH8cndU3H5apFa3bsqa9PjupxeuGX1WLwOkkXqyoGWSoYvqlUAGIq+95/rglX3YVhX2iE=</Modulus><Exponent>AQAB</Exponent><P>8NVPzzcQUAYX5MbiKJNhzN8AOYD8T62Eb4wagjUeKXEXnn2pvtWMXv40+9HzToJqL8/7ifHn9lVtWx/vKqixFw==</P><Q>2mSz3raYQDuXm8civdBlVr+9dyqkWcqxArWPVh1UvCFuNSRFRQlSbY6rrLNmEKovWfF3fPpjCB/YhWUsUfWhhw==</Q><DP>eIZoYI1wDnxeLNITkLbhdQQNOroAi2eNJb357jvjEl5Usa616pk4nLmfjn7twtalibQOcGSt76ulBLugl7rAlQ==</DP><DQ>QU0TK2jVpX8sGfw1OyRw6Lt0X4l0kxS0UOcQ8OLcT1R/q4wo2jerWe7rDjymLB13AyHcZJZJvkgnaduSoP8dSQ==</DQ><InverseQ>m3cVbDJ2ZoFmA9ZwCCpHZXkYWlvoL02jGAe8QprocgLmGZpgOnNJEfkGaubiiRLM4roVlwpXmUfHW7ygBljdDQ==</InverseQ><D>JNa0frrh7b4cBt/fNSzdi69NiHvLxvomoDyTkGJuZzQyDBOJjDXP1MeSgVWSO7GPlgoNq+x/dvIqQGGmbsDXiUXEb/5kQt8OI37DLvZYQgly+VkiwIwyiOghy/52izxMmti/2hnre6vMWgzQy9yp4Q4AWp8rXEbLM4X/imUefB0=</D></RSAKeyValue>";
+
+        string enkey = @"<RSAKeyValue><Modulus>w5iT80FOe4jERmTYR2rejY1V+jFHE5Sxujoa3ApLe4becAeauEfCdyv1yfNxEtFQDchXUMg+3P7xGvtkt4f785toewkMhYsrCyXsxlR5JzbTUwFvhhZEgPQmcSSK3Ugk5LWE40AihXjeCJ2G8Z4M1Tb5s+38bxZTeu7ewyNWx2k=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+
+
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_jiemi_Click(object sender, EventArgs e)
+        {
+            string enstr = this.tb_jiami.Text;
+
+           this.tb_jiemi.Text=  Winner.Framework.Encrypt.RSAProvider.Decrypt(enstr, Encoding.UTF8, false, dekey);
+        }
+
+        private void btn_jiami_Click(object sender, EventArgs e)
+        {
+            string enstr = this.tb_jiemi.Text;
+
+            this.tb_jiami.Text = Winner.Framework.Encrypt.RSAProvider.Encrypt(enstr, Encoding.UTF8, false, enkey);
         }
     }
 }
